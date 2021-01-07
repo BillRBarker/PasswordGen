1feab6f38638783c36c1db4d96aa83b34c873489
brbsgnl <billrbarker@gmail.com>
2021-01-07 13:16:12 -0500

    edited read.md

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# PasswordGen
+# **PasswordGen**
+
+## **Description**
+This web site is used to generate a random password based on choices made by the user (e.g. whether to include upper case, lower case, numbers, or special characters.)
+
+[https://billrbarker.github.io/PasswordGen/](https://billrbarker.github.io/PasswordGen//)
+
+ 
+![Website Capture](/assets/images/Web-site-Capture.PNG)
+
+## **License**
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <https://unlicense.org>
+
+![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/BillRBarker/Bill-Barker-Portfolio)
+
+[![Contributor Covenant](https://img.shields.io/badge/Contributor%20Covenant-v2.0%20adopted-ff69b4.svg)](code_of_conduct.md)
--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+let upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 let lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 let numerals = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 let specialChar = [' ', '!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '>', '=', '>', '?', '@', '[', ']', '^', '_', '`', '{', '}', '|', '~'];
@@ -22,40 +22,45 @@ function writePassword() {
 
 // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", function(){
-  passwordLength = prompt("How long to you want your password to be?");
+generateBtn.addEventListener("click", function () {
+  passwordLength = prompt("How long to you want your password to be? Choose between 8 and 128");
   upperQ = confirm("Do you want upper case letters?");
   lowerQ = confirm("Do you want lower case letters?");
   nummeralQ = confirm("Do you want numberals?");
   specialQ = confirm("Do you want special characters?");
-  console.log (passwordLength);
-  console.log (upperQ);
-  console.log (lowerQ);
-  console.log (nummeralQ);
-  console.log (specialQ);
-  
+  console.log(passwordLength);
+  console.log(upperQ);
+  console.log(lowerQ);
+  console.log(nummeralQ);
+  console.log(specialQ);
+
   if (upperQ === true) {
 
     charPool = upperCase.concat();
-    console.log (charPool);
+    console.log(charPool);
   }
 
   if (lowerQ === true) {
     charPool = charPool.concat(lowerCase);
-    console.log (charPool);
+    console.log(charPool);
   }
 
   if (nummeralQ === true) {
     charPool = charPool.concat(numerals);
-    console.log (charPool);
+    console.log(charPool);
   }
 
   if (specialQ === true) {
     charPool = charPool.concat(specialChar);
-    console.log (charPool);
+    console.log(charPool);
   }
-  
-  
+
+  if (Number(passwordLength >= 8) && Number(passwordLength <= 128)) {
+    for (var i = 0; i < passwordLength; i++) { newPassword += passwordCharset.charAt(Math.floor(Math.random() * passwordCharset.length)); }; localStorage.setItem("password", newPassword);
+    var newPasswordStr = localStorage.getItem("password");
+    passwordE1.textContent = newPasswordStr;
+  };
+
 }
 );
 
